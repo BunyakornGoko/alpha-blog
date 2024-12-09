565837d1f25690c6c50ff76b6ea914ef54b7788b
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-12-09 18:43:17 +0700

    fix-bux-success

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_08_124633) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_08_125807) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
