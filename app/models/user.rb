01e150c14a6eaaf4f47749d6a870d2bfa19ea3c3
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-12-16 23:54:45 +0700

    try-method-before_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/
