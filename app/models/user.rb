248973bfc109843a84814d212fd3dacee40f06a6
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-12-20 21:33:36 +0700

    Add edit template and update action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
     has_many :articles
+    has_secure_password
+  
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  
     VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
-    has_secure_password
-end
+  
+    # Add custom password validation
+    validates :password, presence: true, length: { minimum: 6 }, if: :validate_password?
+  
+    private
+  
+    # Ensure password is validated on create or if explicitly updated
+    def validate_password?
+      new_record? || password.present?
+    end
+  end
+  
