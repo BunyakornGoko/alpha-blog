303e37490b7411e9b3c40776c7011b30500c1d57
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-12-30 15:04:02 +0700

    Add admin into users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_16_180037) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_30_075728) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,5 +25,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_16_180037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
